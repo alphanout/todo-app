260775eedf6da6ab2e355c1521b1fe51b8f326db
alphanout <atishay818@gmail.com>
2021-04-10 14:56:01 +0530

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  
+</template>
+
+<script type = "text/javascript" >
+export default {
+  name: "todo",
+  data() {
+    return {
+      todos: [
+        {
+          title: "Todo A",
+          project: "Project A",
+          done: false,
+        },
+        {
+          title: "Todo B",
+          project: "Project B",
+          done: true,
+        },
+        {
+          title: "Todo C",
+          project: "Project C",
+          done: false,
+        },
+        {
+          title: "Todo D",
+          project: "Project D",
+          done: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+</style>
